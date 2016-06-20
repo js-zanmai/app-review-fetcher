2165bbbc5ea03fa49daae0b0c80c927b42b21f0c
Yoshitomo Minami <minami530@gmail.com>
2016-06-20 23:59:26 +0900

    added unit test

--- test/utility-test.js
+++ test/utility-test.js
@@ -2,16 +2,23 @@ var expect = require('chai').expect;
 var util = require('../utility.js');
 
 describe('utility', function() {
-  it('第二引数に指定した文字数となるように左側から0埋めする。', function() {
-    expect(util.zeroPadding('5', 1)).to.equal('5');
-    expect(util.zeroPadding('5', 2)).to.equal('05');
-    expect(util.zeroPadding('5', 3)).to.equal('005');
-    expect(util.zeroPadding('0', 1)).to.equal('0');
-    expect(util.zeroPadding('05', 2)).to.equal('05');
+  describe('#zeroPadding()', function() {
+    it('第二引数に指定した文字数となるように左側から0埋めする。', function() {
+      expect(util.zeroPadding('5', 2)).to.equal('05');
+      expect(util.zeroPadding('5', 3)).to.equal('005');
+    });
+
+    it('第二引数に指定した文字数と入力された文字数が同じであれば、変更しない。', function() {
+      expect(util.zeroPadding('5', 1)).to.equal('5');
+      expect(util.zeroPadding('0', 1)).to.equal('0');
+      expect(util.zeroPadding('05', 2)).to.equal('05');
+    });
   });
-    
-  it('昨日の時刻情報を取得する。', function() {
-    var now = new Date();
-    expect(util.getYesterday().getDate()).to.equal(now.getDate() - 1);
+  
+  describe('#getYesterday()', function() {
+    it('昨日の時刻情報を取得する。', function() {
+      var now = new Date();
+      expect(util.getYesterday().getDate()).to.equal(now.getDate() - 1);
+    });
   });
 });
