bc3485694f1ff5e061d901bdcad7b4b49e2510e2
Yoshitomo Minami <minami530@gmail.com>
2016-08-13 22:40:04 +0900

    Improved scraping function

--- src/scraper.js
+++ src/scraper.js
@@ -1,3 +1,6 @@
+import 'babel-polyfill';// for async/await
+import requestLib from 'request';
+import cheerio from 'cheerio';
 import client from 'cheerio-httpcli';
 import util from './utility';
 import Review from './review';
@@ -88,5 +91,84 @@ export default class Scraper {
       });
     });
   }
-  
+
+  async fetchReviewFromGooglePlay2(id) {
+
+    const doRequest = (params) => {
+      return new Promise((resolve, reject) => {
+        requestLib(params, (error, response, body) => {
+          if (error) {
+            this.logger.error(error);
+            reject(error);
+          } else {
+            resolve(body);
+          }
+        });
+      }); 
+    };
+
+    const parse = ($) => {
+      const reviews = [];
+      $('div.review-link').remove();
+      $('.single-review').each((i, element) => {
+        const reviewInfo = $(element).find('.review-info');
+        const id = $(element).find('.review-header').attr('data-reviewid');
+        const tmpDate = $(reviewInfo).find('.review-date').text().match(/(.*)年(.*)月(.*)日/);
+        const updated = tmpDate[1] + '/' + util.zeroPadding(tmpDate[2]) + '/' + util.zeroPadding(tmpDate[3]);
+        const rating = $(reviewInfo).find('.review-info-star-rating .tiny-star').attr('aria-label').match(/5つ星のうち(.*)つ星で評価しました/)[1];
+        const reviewBody = $(element).find('.review-body.with-review-wrapper');
+        const title = $(reviewBody).find('.review-title').text();
+        const content = $(reviewBody).text().replace(title, '').trim();
+        const author = $(element).find('.author-name > a').text();
+        
+        reviews.push(new Review(id, updated, title, content, rating, '-', author));
+      });
+
+      return reviews;
+    };
+
+    const fetchBody = (id, page) => {
+      return new Promise((resolve, reject) => {
+        const params = {
+          method: 'POST',
+          uri: 'https://play.google.com/store/getreviews',
+          form: {
+            pageNum: page,
+            id: id,
+            reviewSortOrder: 0,
+            hl: 'jp',
+            reviewType: 0,
+            xhr: 1
+          },
+          json: true
+        };
+        
+        doRequest(params)
+          .then((body) => {
+            const content = JSON.parse(body.slice(6));
+            return content[0][2];
+          })
+          .then(cheerio.load)
+          .then(parse)
+          .then(resolve)
+          .catch(reject);
+      });
+    };
+
+    const allReviews = [];
+    const reviewCountPerPage = 40;// 1リクエストあたり最大40件取得できる。
+    let tmpReviews = [];
+    let page = 0;
+    
+    do {
+      tmpReviews = await fetchBody(id, page);
+      for (const review of tmpReviews) {
+        allReviews.push(review);
+      }
+      page++;
+    } while (tmpReviews.length == reviewCountPerPage);
+    
+    return allReviews;
+  }
+
 }
